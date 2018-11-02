3dc00e7d47ae054f71369ff250e2eb11547434ca
Jaman Salique <jaman.salique@kcl.ac.uk>
2018-11-02 21:36:29 +0000

    Add observable lists to main class.

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,18 +2,28 @@ package application;
 	
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
+import application.model.DirectedNonWeightedGraph;
+import application.model.DirectedWeightedGraph;
 import application.model.UndirectedNonWeightedGraph;
 import application.model.UndirectedWeightedGraph;
 import application.model.Vertex;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	
+	private ObservableList<UndirectedNonWeightedGraph> undirectedNonWeightedGraphData = FXCollections.observableArrayList();
+	private ObservableList<UndirectedWeightedGraph> undirectedWeightedGraphData = FXCollections.observableArrayList();
+	private ObservableList<DirectedNonWeightedGraph> directedNonWeightedGraphData = FXCollections.observableArrayList();
+	private ObservableList<DirectedWeightedGraph> directedWeightedGraphData = FXCollections.observableArrayList();
+	private ObservableList<Vertex> vertexData = FXCollections.observableArrayList();
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -42,39 +52,61 @@ public class Main extends Application {
 		Vertex<String> v2 = new Vertex<String>("a");
 		Vertex<Integer> v3 = new Vertex<Integer>(3);
 		Vertex<Integer> v4 = new Vertex<Integer>(3);
-		UndirectedNonWeightedGraph<String> g = new UndirectedNonWeightedGraph<String>();
-		UndirectedWeightedGraph<String> gg = new UndirectedWeightedGraph<String>();
-//		g.addVertex("a");
-//		g.addVertex("b");
-//		g.addVertex("c");
-//		g.addVertex("d");
-//		
-//		g.addEdge("a","b");
-//		
-//		
-//		System.out.println(g);
 		
-		gg.addVertex("a");
-		gg.addVertex("b");
-		gg.addVertex("c");
-		gg.addVertex("d");
+		UndirectedWeightedGraph<String> uw = new UndirectedWeightedGraph<String>();
+		DirectedWeightedGraph<String> dw = new DirectedWeightedGraph<String>();
+		UndirectedNonWeightedGraph<String> unw = new UndirectedNonWeightedGraph<String>();
+		DirectedNonWeightedGraph<String> dnw = new DirectedNonWeightedGraph<String>();
 		
-		gg.addEdge("a","b",1);
-		gg.addEdge("b","c",2);
-		gg.addEdge("c","d",3);
+		unw.addVertex("a");
+		unw.addVertex("b");
+		unw.addVertex("c");
+		unw.addVertex("d");
+		unw.addEdge("a","b");
+		unw.addEdge("b","c");
+		unw.addEdge("c","d");
+		unw.addEdge("d","c");
+		unw.removeEdge("c","d");
 		
-		//System.out.println(gg);
+		dnw.addVertex("a");
+		dnw.addVertex("b");
+		dnw.addVertex("c");
+		dnw.addVertex("d");
+		dnw.addEdge("a","b");
+		dnw.addEdge("b","c");
+		dnw.addEdge("c","d");
+		dnw.addEdge("d","c");
+		dnw.removeEdge("a","b");
+		
+		uw.addVertex("a");
+		uw.addVertex("b");
+		uw.addVertex("c");
+		uw.addVertex("d");
+		uw.addEdge("a","b",1);
+		uw.addEdge("b","c",2);
+		uw.addEdge("c","d",3);
+		uw.addEdge("d","c",4);
+		uw.removeEdge("d","c");
+		
+		dw.addVertex("a");
+		dw.addVertex("b");
+		dw.addVertex("c");
+		dw.addVertex("d");
+		dw.addEdge("a","b",1);
+		dw.addEdge("b","c",2);
+		dw.addEdge("c","d",3);
+		dw.addEdge("d","c",4);
+		dw.removeEdge("b", "a");
+		
+		System.out.println(unw);
+		System.out.println(dnw);
+		
+		System.out.println(uw);
+		System.out.println(dw);
+		
 		System.out.println(v1.equals(v2));
 		System.out.println(v3.equals(v4));
 		
-//		HashMap<Vertex<Integer>,Integer> adjacencyList = new HashMap<Vertex<Integer>,Integer>();
-//		adjacencyList.put(v1,1);
-//		adjacencyList.put(v2,1);
-//		adjacencyList.put(v3,1);
-//		adjacencyList.put(v4,1);
-//		
-//		adjacencyList.
-//		System.out.println(containsVertex(1, adjacencyList));
 		
 	}
 }
