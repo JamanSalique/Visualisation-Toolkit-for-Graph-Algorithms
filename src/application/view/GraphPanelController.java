2145918c0cb14fc07f6658c671c2777fecfa5731
Jaman Salique <jaman.salique@kcl.ac.uk>
2018-11-03 20:01:44 +0000

    Create controller class for GraphPanel fxml view

---
+++ src/application/view/GraphPanelController.java
@@ -0,0 +1,18 @@
+package application.view;
+
+import application.Main;
+import javafx.fxml.FXML;
+
+public class GraphPanelController {
+
+	private Main main;
+	
+	@FXML
+	private void initialize() {
+		
+	}
+	
+	public void setMain(Main main) {
+		this.main = main;
+	}
+}
